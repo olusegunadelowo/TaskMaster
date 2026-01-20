3557b20f8eda869db8415b2ef98723b8dc5af5c4
Olusegun <olusegun.adelowo@yahoo.com>
2026-01-20 12:54:09 +0100

    4th commit

--- TaskMaster/core/urls.py
+++ TaskMaster/core/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .views import home
 
-urlpatterns = [path('home/', home, name='home'),]
+urlpatterns = [path('home/', home, name='homepage'),]
--- TaskMaster/core/views.py
+++ TaskMaster/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hello world')
+    return render (request, 'core/home.html')        
